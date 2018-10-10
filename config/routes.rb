88520c667b3254db0b7f6728a66c30dec0ebab66
nalabjp <nalabjp@gmail.com>
2018-10-10 15:40:57 +0900

    Add unauthorized top page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  root 'home#index'
+  authenticated :user do
+    root 'home#index'
+  end
+  root 'no_auth_home#index'
+
   resources :users, only: %i(index)
 end
