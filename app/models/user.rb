2514f0364ae5fab9a6e1aefca0d968ba47b30640
nalabjp <nalabjp@gmail.com>
2018-10-10 11:59:43 +0900

    Remove duplicated configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :invitable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
