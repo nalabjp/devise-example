79950d34ff0700d580967519ad0de384ed50bba2
nalabjp <nalabjp@gmail.com>
2018-10-19 20:55:17 +0900

    Inherit instead of delegating to `LegacyUser`
    Fix `#is_a?(User)` problem

--- app/models/legacy_user.rb
+++ app/models/legacy_user.rb
@@ -5,37 +5,4 @@ class LegacyUser < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_one :legacy_user_information
-
-  ## For devise compatible with User
-
-  # https://github.com/plataformatec/devise/blob/715192a7709a4c02127afb067e66230061b82cf2/test/mapping_test.rb#L76-L80
-  def devise_scope
-    :user
-  end
-
-  class << self
-    def new(*)
-      super.to_user
-    end
-  end
-
-  def dup
-    super.to_user
-  end
-
-  def clone
-    super.to_user
-  end
-
-  def init_with(*)
-    super.to_user
-  end
-
-  # The expected return value for behaviors delegated from initialization of User is an instance of User
-  def to_user
-    User.allocate.tap { |u| u.initialize_with_legacy_user(self) }
-  end
 end
-
-# We can not delegate to a protected method, so change it to a public method
-LegacyUser.protected_instance_methods.each { |method| LegacyUser.class_eval { public method } }
